af833aef96cfdf579abd2e7187b83cf3106fb617
Dhiraj Acharya <076bct023.dhiraj@pcampus.edu.np>
2024-05-05 10:20:00 +0545

    add task count functionality

--- script.js
+++ script.js
@@ -7,6 +7,15 @@ const searchBox = document.getElementById("search_box");
 const addBtn = document.getElementById("add_btn");
 const todoLists = document.querySelector(".todo_lists");
 const errorBox = document.getElementById("error_box");
+const taskCount = document.getElementById("task_count");
+
+let countItem = 0;
+taskCount.innerText = countItem;
+
+
+function updateCount(){
+    taskCount.innerText = countItem;
+}
 
 function showErrorMessage() {
     errorBox.style.display = "block";
@@ -32,8 +41,12 @@ function addTodoItem() {
 
         todoLists.appendChild(itemDiv);
         searchBox.value = "";
+        
+        ++countItem;
+        updateCount();
 
         saveItems(); //saves todo item to localStorage;
+
     }
 }
 
@@ -60,35 +73,58 @@ function toggleTodo(e) {
         e.target.nextSibling.style.textDecoration = "line-through";
         e.target.nextSibling.style.textDecorationColor = "white";
 
+        --countItem;
+        updateCount();
+
+
     } else {
         e.target.classList.remove("fa-solid", "fa-square-check");
         e.target.classList.add("fa-regular", "fa-square");
 
         e.target.nextSibling.style.textDecoration = "none";
+
+        ++countItem;
+        updateCount();
     }
 
 }
 
 todoLists.addEventListener("click", (e) => {
+    let striked_P;
 
     if (e.target.classList.contains("first")) {
         toggleTodo(e);
         saveItems(); //saves todo item to localStorage;
-
     }
 
     if (e.target.classList.contains("last")) {
+        
+        striked_P = e.target.previousElementSibling.previousElementSibling;
+        
+        //text strike through xaina vaney matrai count update garni, text striketrhough huda aafai count ghati sakeko hunxa
+        if(striked_P.style.textDecorationColor!=="white"){ 
+              --countItem;
+               updateCount();
+        }
+        
         e.target.parentNode.remove();
+        
         saveItems(); //saves todo item to localStorage;
     }
 
     if (e.target.classList.contains("second")) {
-        searchBox.value = e.target.previousSibling.textContent;
+        searchBox.value = e.target.previousElementSibling.textContent;
+
+        striked_P = e.target.previousElementSibling;
+        if(striked_P.style.textDecorationColor!=="white"){
+            --countItem;
+            updateCount();
+        }
+
         e.target.parentNode.remove();
         searchBox.focus();
+
         saveItems(); //saves todo item to localStorage;
-
-
     }
 
 })
@@ -97,11 +133,18 @@ todoLists.addEventListener("click", (e) => {
 // start: local storage related
 function saveItems() {
     localStorage.setItem("data", todoLists.innerHTML);
+    localStorage.setItem("count", countItem);
 }
 
 
 function showItems() {
     todoLists.innerHTML = localStorage.getItem("data");
+
+    countItem = localStorage.getItem("count");
+    if(countItem!==null){
+        updateCount();
+    }
+
 }
 
 showItems();
